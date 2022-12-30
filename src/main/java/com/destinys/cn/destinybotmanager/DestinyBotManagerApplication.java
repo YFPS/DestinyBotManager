fe2730d24b291255265d5fa42243a9656ce617b8
M4 <2859098803@qq.com>
2022-12-30 20:25:43 +0800

    Initial commit

---
+++ src/main/java/com/destinys/cn/destinybotmanager/DestinyBotManagerApplication.java
@@ -0,0 +1,13 @@
+package com.destinys.cn.destinybotmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DestinyBotManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DestinyBotManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/destinys/cn/destinybotmanager/DestinyBotManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.destinys.cn.destinybotmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DestinyBotManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
